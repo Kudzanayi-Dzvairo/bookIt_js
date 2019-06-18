37ae83aa46dd690617ca542e05a1ddb2da55f57c
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-06-18 12:12:19 -0400

    single rooms component

--- src/context.js
+++ src/context.js
@@ -28,10 +28,17 @@ export default class RoomProvider extends Component {
     });
     return tempItems;
   }
+
+  getRoom = slug => {
+    let tempRooms = [...this.state.rooms];
+    const room = tempRooms.find(room => room.slug === slug);
+    return room;
+  };
+
+  
   render() {
-    console.log(this.state.rooms);
     return (
-      <RoomContext.Provider value={{ ...this.state }}>
+      <RoomContext.Provider value={{ ...this.state, getRoom: this.getRoom }}>
         {this.props.children}
       </RoomContext.Provider>
     );
