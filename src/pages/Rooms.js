a9e1f8081c90b65e1371c48828a6ad94033844ef
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-06-18 21:13:53 -0400

    filter completed

--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -2,16 +2,20 @@ import React from "react";
 import Hero from "../components/Hero";
 import Banner from "../components/Banner";
 import { Link } from "react-router-dom";
+import RoomsContainer from "../components/RoomContainer";
 
 const Rooms = (children, hero) => {
   return (
-    <Hero hero="roomsHero">
-      <Banner title="our rooms">
-      <Link to="/" className="btn-primary">
-        Home
-      </Link>
-      </Banner>
-    </Hero>
+    <>
+      <Hero hero="roomsHero">
+        <Banner title="our rooms">
+          <Link to="/" className="btn-primary">
+            Home
+          </Link>
+        </Banner>
+      </Hero>
+      <RoomsContainer />
+    </>
   );
 };
 
